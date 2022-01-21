6d8a968b8916d0f954163cad1249d914a0d233c9
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-01-21 17:01:53 +0800

    back_end

---
+++ back_end_springboot/src/main/java/com/demo/back_end_springboot/back_end_springboot/BackEndSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.demo.back_end_springboot.back_end_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSpringbootApplication.class, args);
+	}
+
+}
