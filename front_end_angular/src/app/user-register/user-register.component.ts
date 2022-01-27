9812c7d47fec725490d0116209dbabe446b09564
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-01-27 15:56:56 +0800

    1. front_end register form rebulid to reactive form 2. front_end register form add valid rule

--- front_end_angular/src/app/user-register/user-register.component.ts
+++ front_end_angular/src/app/user-register/user-register.component.ts
@@ -1,8 +1,11 @@
+import { map, tap } from 'rxjs';
 import { User } from './../model/user';
 import { UserService } from './../user.service';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidationErrors, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserAccountValidator } from '../share/validators/user-account-validator';
+import { pwdValidator } from '../share/validators/pwd.directive';
 
 @Component({
   selector: 'app-user-register',
@@ -11,14 +14,34 @@ import { Router } from '@angular/router';
 })
 export class UserRegisterComponent implements OnInit {
 
+  registerForm:FormGroup = new FormGroup({});
+  private userAccountValidator:UserAccountValidator = new UserAccountValidator(this.userService).set('account');
+  private userMailValidator:UserAccountValidator = new UserAccountValidator(this.userService).set('mail');
+  private userPhoneValidator:UserAccountValidator  = new UserAccountValidator(this.userService).set('phone');
+
   constructor(private userService:UserService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+
+  private createForm():void {
+    const emailRegex = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+    const phoneRegex = /^09\d{2}-?\d{3}-?\d{3}$/;
+    this.registerForm = this.formBuilder.group({
+      account: ['', [Validators.required], [this.userAccountValidator.validate.bind(this.userAccountValidator)]],
+      pwd: ['', Validators.required],
+      pwd2nd: ['', Validators.required],
+      mail: ['', [Validators.required, Validators.pattern(emailRegex)], [this.userMailValidator.validate.bind(this.userMailValidator)]],
+      phone: ['', [Validators.required, Validators.pattern(phoneRegex)], [this.userPhoneValidator.validate.bind(this.userPhoneValidator)]],
+    }, {validators: pwdValidator });
+}
+
   // sign up submit
-  public onRegisterSubmit(registerForm:NgForm):void {
+  public onRegisterSubmit(registerForm:FormGroup):void {
 
     if (registerForm.value.pwd != registerForm.value.pwd2nd) {
       alert(`pwd2nd is false`);
@@ -27,7 +50,6 @@ export class UserRegisterComponent implements OnInit {
 
     this.userService.registerUser(registerForm.value).subscribe(
       (res : User) => {
-
         if (res.message.indexOf("已註冊過") != -1) {
 
           alert(false);
@@ -39,7 +61,4 @@ export class UserRegisterComponent implements OnInit {
       }
     );
   }
-
-
-
 }
