e83ea941b6f04cb244148f64e90ba506d2021d1e
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-01-27 15:55:51 +0800

    front_end login form rebulid to reactive form

--- front_end_angular/src/app/login-index/login-index.component.ts
+++ front_end_angular/src/app/login-index/login-index.component.ts
@@ -1,6 +1,6 @@
 import { UserService } from './../user.service';
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,26 +10,35 @@ import { Router } from '@angular/router';
 })
 export class LoginIndexComponent implements OnInit {
 
+  loginForm: FormGroup = new FormGroup({});
+
   constructor(private userService:UserService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
-  public onLoginSubmit(loginForm : NgForm) {
+  private createForm():void {
+      this.loginForm = this.formBuilder.group({
+        account:['', Validators.required],
+        pwd:['', Validators.required]
+      });
+  }
+
+  public onLoginSubmit(loginForm : FormGroup):void {
     this.userService.validUser(loginForm.value).subscribe(
       (res:Boolean) => {
         if (res) {
           // 驗證成功
           alert(res);
           this.router.navigate(['home']);
-
         } else {
           // 驗證fail
           alert (res + "登入失敗");
         }
       }
     )
-    loginForm.reset();
   }
 }
