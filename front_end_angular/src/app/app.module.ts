1163599b4d55018b2415d0fd50ee635fbaf0ab12
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-01-27 15:53:31 +0800

    rebulid login&register form to angular reactive form (for validation use)

--- front_end_angular/src/app/app.module.ts
+++ front_end_angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +20,8 @@ import { UserRegisterComponent } from './user-register/user-register.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
