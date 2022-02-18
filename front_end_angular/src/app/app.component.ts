08d5b661e45199b71c5af7d3b486070622e019f9
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-02-18 09:52:45 +0800

    front_end  appComponent補constructor(for show hide login logout)

--- front_end_angular/src/app/app.component.ts
+++ front_end_angular/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front_end_angular';
+  constructor( public authService:AuthService ) {
+
+  }
 }
