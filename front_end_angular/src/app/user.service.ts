29d6e02e3cfd3a29d7114f0242007d7ccf00af96
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-02-18 09:46:51 +0800

    front_end 移除login_validmethod 調整register url

--- front_end_angular/src/app/user.service.ts
+++ front_end_angular/src/app/user.service.ts
@@ -8,31 +8,20 @@ import { User } from './model/user';
 })
 export class UserService {
 
-  private loginStatus:Boolean = false;
-
   constructor(private http: HttpClient) { }
-
-
-  public validUser(user:User) : Observable<Boolean> {
-    let rtn = this.http.post<Boolean>(`api/user/loginValid`, user);
-
-    rtn.pipe(tap((data:Boolean) => {this.loginStatus = data}));
-
-    return rtn;
-  }
 
   public registerUser(user:User) : Observable<User> {
     return this.http.post<User>(`api/user/register`, user);
   }
 
   public validUserAccount (account:string) : Observable<Boolean> {
-    return this.http.post<Boolean>(`api/user/isAlreadyHaveAccount`, account);
+    return this.http.post<Boolean>(`api/user/register/isAlreadyHaveAccount`, account);
   }
   public validUserMail (mail:string) : Observable<Boolean> {
-    return this.http.post<Boolean>(`api/user/isAlreadyHaveMail`, mail);
+    return this.http.post<Boolean>(`api/user/register/isAlreadyHaveMail`, mail);
   }
   public validUserPhone (phone:string) : Observable<Boolean> {
-    return this.http.post<Boolean>(`api/user/isAlreadyHavePhone`, phone);
+    return this.http.post<Boolean>(`api/user/register/isAlreadyHavePhone`, phone);
   }
 
 
