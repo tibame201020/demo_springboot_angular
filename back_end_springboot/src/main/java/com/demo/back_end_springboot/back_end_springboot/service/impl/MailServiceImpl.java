df2bd94452fad79ac3e2502d536bf3fb37152966
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-02-23 15:30:04 +0800

    back_end實作mailServie send validMail

--- back_end_springboot/src/main/java/com/demo/back_end_springboot/back_end_springboot/service/impl/MailServiceImpl.java
+++ back_end_springboot/src/main/java/com/demo/back_end_springboot/back_end_springboot/service/impl/MailServiceImpl.java
@@ -1,7 +1,9 @@
 package com.demo.back_end_springboot.back_end_springboot.service.impl;
 
+import com.demo.back_end_springboot.back_end_springboot.domain.Auth;
 import com.demo.back_end_springboot.back_end_springboot.domain.User;
 import com.demo.back_end_springboot.back_end_springboot.service.MailService;
+import com.demo.back_end_springboot.back_end_springboot.util.JwtProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -12,16 +14,24 @@ public class MailServiceImpl implements MailService {
 
     @Autowired
     private JavaMailSender mailSender;
+    @Autowired
+    private JwtProvider jwtProvider;
 
     @Override
     public User sendValidMail(User user) {
         SimpleMailMessage message = getSimpleMailMessage(user);
         message.setSubject("Thanks Register Start Ur Journey");
         //todo
-        // this is need to generate a link to valid user
-        message.setText("");
+        String valid_token = jwtProvider.getToken(new Auth(user), 7*24*60*1000, "");
+        //this is need to generate a link to valid user
+        String preStr = "Dear " + user.getAccount() + " :" + "\n";
+
+        preStr = preStr + "plz click the under url to enable ur account" + "\n";
+
+        String base_enable_url = "http://localhost:4200/valid?validToken=";
+        message.setText(preStr + base_enable_url + valid_token);
         sendMail(message);
-        return null;
+        return user;
     }
 
     @Override
@@ -32,7 +42,7 @@ public class MailServiceImpl implements MailService {
         // this is need to generate a link to reset user password
         message.setText("");
         sendMail(message);
-        return null;
+        return user;
     }
 
 
