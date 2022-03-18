8a1b40b3d0f4eb0faea3d0680ca851583d2bcedc
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-03-18 11:03:21 +0800

    front_end add share component candlestick-chart(stock info {股價，基本資訊} )

---
+++ front_end_angular/src/app/share/candlestick-chart/candlestick-chart.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { PublishService } from 'src/app/publish/publish.service';
+
+@Component({
+  selector: 'app-candlestick-chart',
+  templateUrl: './candlestick-chart.component.html',
+  styleUrls: ['./candlestick-chart.component.css']
+})
+export class CandlestickChartComponent implements OnInit {
+
+  @Input() code!: string;
+  dataLs = [];
+  dataRange: any;
+  startDate: any;
+  endDate: any;
+  showHide:boolean = false;
+  constructor(private PublishService: PublishService) { }
+  ngOnInit(): void { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.startDate && this.endDate && changes['code']) {
+      this.getBasicInfo(this.code, this.startDate, this.endDate);
+    }
+  }
+  getBasicInfo(codeNm: string, startDate: any, endDate: any) {
+    if (codeNm.trim().indexOf(' - ') != -1) {
+      codeNm = codeNm.trim().split(' - ')[0].trim();
+    }
+    if (codeNm.trim().indexOf('-') != -1) {
+      codeNm = codeNm.trim().split('-')[0].trim();
+    }
+    this.PublishService.getBasicInfo(codeNm, startDate, endDate).subscribe(
+      res => {
+        console.log(res)
+        this.showHide = res.status;
+        this.dataLs = res;
+      }
+    );
+  }
+
+  getDataRange(dataRange: any) {
+    this.dataRange = dataRange;
+    const startDate = this.dataRange.start ? this.dataRange.start.toLocaleString('zh-TW', { year: "numeric", month: "2-digit", day: "2-digit" }) : '';
+    const endDate = this.dataRange.end ? this.dataRange.end.toLocaleString('zh-TW', { year: "numeric", month: "2-digit", day: "2-digit" }) : '';
+    this.startDate = startDate;
+    this.endDate = endDate;
+
+    if (startDate && endDate && this.code) {
+      this.getBasicInfo(this.code, startDate, endDate);
+    }
+  }
+}
