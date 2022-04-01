7148cd8079398dd6c8454a6e7daf167afc7f90b5
JEFFHSU\JeffHsu <jeff.hsu@promeritage.com.tw>
2022-04-01 14:49:49 +0800

    back_end add get article

--- back_end_springboot/src/main/java/com/demo/back_end_springboot/back_end_springboot/repo/ArticleRepo.java
+++ back_end_springboot/src/main/java/com/demo/back_end_springboot/back_end_springboot/repo/ArticleRepo.java
@@ -2,9 +2,11 @@ package com.demo.back_end_springboot.back_end_springboot.repo;
 
 import com.demo.back_end_springboot.back_end_springboot.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ArticleRepo extends JpaRepository<Article, Integer> {
     List<Article> findByAccount(String account);
+    List<Article> findByStateAndVisibilityOrderByUpdateTimeDesc(String state, String visibility);
 }
--- back_end_springboot/src/main/java/com/demo/back_end_springboot/back_end_springboot/service/impl/ArticleServiceImpl.java
+++ back_end_springboot/src/main/java/com/demo/back_end_springboot/back_end_springboot/service/impl/ArticleServiceImpl.java
@@ -28,5 +28,10 @@ public class ArticleServiceImpl implements ArticleService {
         return articleRepo.findByAccount(account);
     }
 
+    @Override
+    public List<Article> findByPublish() {
+        return articleRepo.findByStateAndVisibilityOrderByUpdateTimeDesc("publish", "all");
+    }
+
 
 }
