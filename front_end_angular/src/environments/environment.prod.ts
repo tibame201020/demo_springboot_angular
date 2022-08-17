a823593819b28cb509c70dd21bbe59808f6eaa8a
tibame201020 <tibame201020@gmail.com>
2022-08-17 11:22:55 +0800

    run script.sh can run on local for all

--- front_end_angular/src/environments/environment.prod.ts
+++ front_end_angular/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   backend: {
-    baseURL:"https://dockerize-backend0811.herokuapp.com/"
+    baseURL:"http://localhost:8080/"
+    // baseURL:"https://dockerize-backend0811.herokuapp.com/"
     // baseURL:"http://192.168.16.128:8080/"
     // baseURL:"https://demo0408.herokuapp.com/"
   }
